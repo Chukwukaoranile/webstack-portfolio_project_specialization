3a4362444ff6738346c994056d991cbf8089ee3f
Chidimma Onuora <onuorachidimma4@gmail.com>
2023-10-19 17:27:07 +0100

    styled the nav sidebar

--- ecommerce/templates/assets/js/side-cart.js
+++ ecommerce/templates/assets/js/side-cart.js
@@ -1,7 +1,7 @@
 let cartSidebar = document.getElementById('cartBag')
 let mobileMenu = document.getElementById('openCart1')
 let closeX = document.getElementById('x')
-let openSidebar = false;
+
 
 
 cartBag.addEventListener("click", () => {
@@ -14,4 +14,16 @@ closeX.addEventListener("click", () => {
     openCart1.style.display = "none"; // Hide the section
 });
 
+let navSidebar = document.getElementById('navOpen')
+let sideNav = document.getElementById('nav-sidebar')
+let closeNav = document.getElementById('nav-x')
 
+navOpen.addEventListener("click", () => {
+    event.preventDefault();
+    sideNav.style.display = "block"; // Show the section
+  });
+
+closeNav.addEventListener("click", () => {
+    event.preventDefault();
+    sideNav.style.display = "none"; // Hide the section
+});
